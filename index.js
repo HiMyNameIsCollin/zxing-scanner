d2a96b5aac799be28645e7eb8824f7b8bc49e002
HiMyNameIsCollin <collinstone2291@icloud.com>
2024-05-28 08:05:32 -0700

    fix on firefox

--- index.js
+++ index.js
@@ -2,28 +2,39 @@ document.addEventListener('DOMContentLoaded', function () {
   const video = document.getElementById('video');
   const { BrowserMultiFormatReader, DecodeHintType, BarcodeFormat } = ZXing;
   const hints = new Map();
-  // hints.set(DecodeHintType.POSSIBLE_FORMATS, [
-  //   BarcodeFormat.UPC_A,
-  //   BarcodeFormat.UPC_E,
-  //   BarcodeFormat.QR_CODE,
-  //   BarcodeFormat.EAN_8,
-  //   BarcodeFormat.EAN_13,
-  //   BarcodeFormat.UPC_EAN_EXTENSION,
-  //   BarcodeFormat.CODE_39,
-  //   BarcodeFormat.CODE_93,
-  //   BarcodeFormat.CODE_128,
-  // ]);
-  // hints.set(DecodeHintType.TRY_HARDER, true); // Fucks up QR
+  hints.set(DecodeHintType.POSSIBLE_FORMATS, [
+    BarcodeFormat.UPC_A,
+    BarcodeFormat.UPC_E,
+    BarcodeFormat.QR_CODE,
+    BarcodeFormat.EAN_8,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.UPC_EAN_EXTENSION,
+    BarcodeFormat.CODE_39,
+    BarcodeFormat.CODE_93,
+    BarcodeFormat.CODE_128,
+  ]);
+  // hints.set(DecodeHintType.TRY_HARDER, true); // Fucks up
   // Function to start the video stream
-  function startVideoStream() {
+  async function startVideoStream() {
+    const codeReader = new BrowserMultiFormatReader();
+
+    let selectedDeviceId;
+
+    const vidDevices = await codeReader.listVideoInputDevices();
+    selectedDeviceId = vidDevices[0].deviceId;
+
     navigator.mediaDevices
       .getUserMedia({
-        video: { facingMode: 'environment' },
+        video: { facingMode: 'environment', torch: true },
       })
       .then((stream) => {
         video.srcObject = stream;
         const videoTrack = stream.getVideoTracks()[0];
-        if (!videoTrack.getCapabilities()?.torch) {
+        console.log(videoTrack.getConstraints());
+        if (
+          !videoTrack?.getCapabilities ||
+          !videoTrack.getCapabilities()?.torch
+        ) {
           window.alert(`${videoTrack?.label} cannot access the flashlight`);
         }
         document
@@ -34,6 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
               .catch((error) =>
                 console.error('Error turning on flashlight:', error),
               );
+            console.log(videoTrack.getConstraints());
           });
 
         document
@@ -46,8 +58,8 @@ document.addEventListener('DOMContentLoaded', function () {
               );
           });
         video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
-        video.play();
-        scanCode();
+        // video.play();
+        scanCode(codeReader, selectedDeviceId);
       })
       .catch((err) => {
         console.error('Error accessing the camera: ', err);
@@ -55,11 +67,10 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Function to scan the code
-  function scanCode() {
-    const codeReader = new BrowserMultiFormatReader();
+  function scanCode(codeReader, selectedDeviceId) {
     codeReader
       .decodeFromVideoDevice(
-        null,
+        selectedDeviceId,
         'video',
         (result, err) => {
           if (result) {
